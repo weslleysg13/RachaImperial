c51e131065ede510ff93bfac89bde46342ed4e64
Weslley SG <weslleysg13@gmail.com>
2023-10-31 21:07:45 -0300

    Novos arquivos heroku

--- novotcc/settings.py
+++ novotcc/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ot*moq_x7-^v#)83z3ao*hklr(zo6_l#^l80ykf&o^)br0+2i1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
